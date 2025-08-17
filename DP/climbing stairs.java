166268e07907c1b9aa84989050df5a08cea6e397
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2025-08-17 20:13:14 +0530

    Update climbing stairs.java

--- DP/climbing stairs.java
+++ DP/climbing stairs.java
@@ -1,2 +1,24 @@
 //  https://leetcode.com/problems/climbing-stairs/
 
+//if n==1 ans=1, {1}
+//if n==2 ans=2, {(1,1), (2)}
+//if n==3 ans=3, {(1,1,1), (2,1),(1,2)}
+//if n==4 ans=4, {(1,1,1,1), (1,2,1),(2,1,1), (1,1,2), (2,2)}
+
+//  for ex. to reach 4 =  (no of ways to reach 3th) or (no of ways to reach 2th)
+//                     =   3 +2  =5
+
+
+public int climbStairs(int n)
+  {      
+        if(n<3) return n;
+        int x=1, y=2, ans=0;
+
+        for(int i=3;i<=n;i++)
+        {
+            ans=x+y;
+            x=y;
+            y=ans;
+        }
+        return ans;
+    }
