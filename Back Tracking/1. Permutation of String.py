279bd954b76065a45f17345814d0573ed1d9a21b
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2025-09-16 23:15:25 +0530

    Create 1. Permutation of String.py

---
+++ Back Tracking/1. Permutation of String.py
@@ -0,0 +1,11 @@
+#  https://leetcode.com/problems/subsets/
+
+                []
+         /                \
+       [1]                []
+      /   \              /   \
+   [1,2]  [1]         [2]     []
+   /  \    / \        / \     / \
+[1,2,3][1,2] [1,3][1] [2,3][2] [3][]
+
+# Lets take example [1,2,3,4]      = it can have 2*2*2*2=16 total subsets
