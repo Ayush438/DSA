5a08cfe1e258d1abf977b922ffd28aaddad431f1
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2025-11-04 23:43:12 +0530

    Create 6. Same.py

---
+++ Tree/6. Same.py
@@ -0,0 +1,11 @@
+# https://neetcode.io/problems/same-binary-tree?list=neetcode250
+
+def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p==None and q==None:
+            return True
+        if p==None or q==None:
+            return False
+        if p.val!=q.val:
+            return False
+        
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
