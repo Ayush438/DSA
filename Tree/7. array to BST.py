9886dfb489ff1bf3834977724a211702a2e954e8
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2025-11-06 23:54:30 +0530

    Update 7. array to BST.py

--- Tree/7. array to BST.py
+++ Tree/7. array to BST.py
@@ -13,3 +13,25 @@ def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         if not nums:
             return None
         return bst(0,len(nums)-1)
+
+
+
+--------------------------------------------------------------------
+# https://neetcode.io/problems/merge-two-binary-trees?list=neetcode250
+
+def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        def merge(root1 ,root2):
+            if root1==None and root2==None:
+                return None
+            
+            if root1==None:
+                return root2
+            if root2==None:
+                return root1
+            
+            node=TreeNode(root1.val+root2.val)
+            node.left=merge(root1.left,root2.left)
+            node.right=merge(root1.right,root2.right)
+            return node
+
+        return merge(root1, root2)
