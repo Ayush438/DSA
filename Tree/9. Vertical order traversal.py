87aab0a57293e3cdbc87b32f579cb68b2ba1fca8
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2025-11-08 22:51:27 +0530

    Refactor vertical order traversal implementation

--- Tree/9. Vertical order traversal.py
+++ Tree/9. Vertical order traversal.py
@@ -8,18 +8,17 @@ def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if root==None:
             return []
 
-        col_table=defaultdict(list)
+        col_no=defaultdict(list)
         queue=deque([(root,0)])
 
         while queue:
-            node, col=queue.pop()
-            col_table[col].append(node.val)
+            node, col=queue.popleft()
 
-            if node.right:
-                queue.append((node.right,col+1))
             if node.left:
                 queue.append((node.left,col-1))
-
-        temp=sorted(col_table.keys())
-
-        return [col_table[col] for col in temp]
+            if node.right:
+                queue.append((node.right,col+1))
+            col_no[col].append(node.val)
+        
+        temp=sorted(col_no.keys())
+        return [col_no[col] for col in temp]
