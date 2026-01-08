9a9a8d1059188535afc186d96f17c89495d9a8c9
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2026-01-09 00:08:31 +0530

    Create Sheet.py

---
+++ Sheet.py
@@ -0,0 +1,10 @@
+Tree:
+
+1. Inorder Traversal of Binary Tree      |
+2. Level Order Traversal                 |
+3. Height of a Binary Tree               |
+4. Balance Binary Tree                   |  Day 1
+5. Diameter of Binary Tree               |
+6. Maximum Path Sum                      |
+7. Same Tree                             |
+8. Zigzag Level Order Traversal          |
