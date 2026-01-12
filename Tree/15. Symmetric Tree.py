4cbf8bb1c2d04eb8baff8ac8346f131e96b15336
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2026-01-12 22:52:43 +0530

    Create 15. Symmetric Tree.py

---
+++ Tree/15. Symmetric Tree.py
@@ -0,0 +1,29 @@
+
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def bfs(root):
+            if not root:
+                return True
+
+            queue=deque([(root.left, root.right)])
+
+            while queue:
+                node1, node2=queue.popleft()
+
+                if not node1 and not node2:
+                    continue
+                if not node1 or not node2:
+                    return False
+                if node1.val!=node2.val:
+                    return False
+
+                queue.append((node1.left, node2.right))  
+                queue.append((node1.right, node2.left))  
+                
+
+            return True
+
+        return bfs(root)
+      
