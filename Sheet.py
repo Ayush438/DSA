0ef6e3e16d8d46bd5b4b8bf722a08caf3ba7965b
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2026-01-20 23:13:54 +0530

    Update Sheet.py

--- Sheet.py
+++ Sheet.py
@@ -1,21 +1,41 @@
 Tree:
 
-1. Inorder Traversal of Binary Tree      |
-2. Level Order Traversal                 |
-3. Height of a Binary Tree               |
-4. Balance Binary Tree                   |  Day 1
-5. Diameter of Binary Tree               |
-6. Maximum Path Sum                      |
-7. Same Tree                             |
-8. Zigzag Level Order Traversal          |
+Requirements needed to construct a unique BT
+                  Inorder + Preorder  	✅
+                  Inorder + Postorder	  ✅
+                  Inorder + Levelorder	✅
+                  Pre + Post (Full BT)	✅
+                  Preorder + Postorder	❌
+                  Preorder only	        ❌
+                  Postorder only      	❌
+                  
+--------------------------------------------------------
+1. Inorder Traversal of Binary Tree                  |
+2. Level Order Traversal                             |
+3. Height of a Binary Tree                           |
+4. Balance Binary Tree                               |  Day 1
+5. Diameter of Binary Tree                           |
+6. Maximum Path Sum                                  |
+7. Same Tree                                         |
+8. Zigzag Level Order Traversal                      |
 
-9. Tree Boundary Traversal               |
-10. Vertical Order Traversal             |
-11. Top View of Binary Tree              | Day 2
-12. Bottom View                          |
-13. Right Side View                      |
-14. Symmetric Tree                       |
-15. Binary Tree Paths                    |
-16. Lowest Common Ancestor               |
+9. Tree Boundary Traversal                           |
+10. Vertical Order Traversal                         |
+11. Top View of Binary Tree                          | Day 2
+12. Bottom View                                      |
+13. Right Side View                                  |
+14. Symmetric Tree                                   |
+15. Root to Node Path                                |
+16. Lowest Common Ancestor                           |
 
-17. Lowest Common Ancestor
+17. Maximum width                                    |
+18. Check for Children Sum Property                  |
+19. Print all the Nodes at a distance K              | Day 3
+20. Minimum time taken to burn the BT from a Node    |
+
+21. Count total Nodes in a COMPLETE Binary Tree      |
+22. Binary Tree from Preorder and Inorder Traversal  |
+23. Binary Tree from Preorder and Postorder Traversal|
+24. Binary Tree from Inorder and Postorder Traversal |
+  
+26. Flatten Binary Tree to Linked List               |
