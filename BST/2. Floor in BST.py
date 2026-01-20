b69f04039fb0f6729cc3580c6842e1bb3fec11dc
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2026-01-20 23:48:07 +0530

    Create 2. Floor in BST.py

---
+++ BST/2. Floor in BST.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def floor(self, root, x):
+        ans=-1
+        
+        while root:
+            if root.data==x:
+                return x
+            
+            if root.data>x:
+                root=root.left
+            else:
+                ans=root.data
+                root=root.right
+        return ans
