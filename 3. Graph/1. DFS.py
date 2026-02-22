5fea9b78eeb33469ab7e5320e537fd9f69021bb8
Ayush438 <71399459+Ayush438@users.noreply.github.com>
2026-02-22 13:32:56 +0530

    Update 1. DFS.py

--- 3. Graph/1. DFS.py
+++ 3. Graph/1. DFS.py
@@ -1,5 +1,7 @@
 #https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
 
+
+# Recursive
 class Solution:
     def dfs(self, adj):
         def traverse(v, adj, visited, ans):
@@ -16,4 +18,28 @@ class Solution:
         ans=[]
         traverse(0,adj,visited,ans)
         return ans
+
+
+# Non Recursive
+
+class Solution:
+    def dfs(self, adj):
+        
+        n = len(adj)
+        visited = [False] * n
+        ans = []  
+        stack = [0]          # start from node 0
+        
+        while stack:
+            v = stack.pop()
             
+            if not visited[v]:
+                visited[v] = True
+                ans.append(v)
+                
+                # push neighbors in reverse order
+                for u in reversed(adj[v]):
+                    if not visited[u]:
+                        stack.append(u)
+        
+        return ans
